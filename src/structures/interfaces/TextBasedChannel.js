f46d0152eda13759ec42718aaa53c9e71d3959d8
Adrián <62357050+Adrian3221@users.noreply.github.com>
2021-03-09 17:00:45 -0500

    Update TextBasedChannel.js

--- src/structures/interfaces/TextBasedChannel.js
+++ src/structures/interfaces/TextBasedChannel.js
@@ -64,7 +64,7 @@ class TextBasedChannel {
    * @property {string|boolean} [code] Language for optional codeblock formatting to apply
    * @property {boolean|SplitOptions} [split=false] Whether or not the message should be split into multiple messages if
    * it exceeds the character limit. If an object is provided, these are the options for splitting the message
-   * @property {UserResolvable} [reply] User to reply to (prefixes the message with a mention, except in DMs)
+   * @property {MessageResolvable} [replyTo] The message to reply to (must be in the same channel)   
    */
 
   /**
